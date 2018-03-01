199363fa0552ab192ccfa57927d661015a0eb163
Mijo Kaliger <mijo.kaliger@gmail.com>
2018-03-01 08:56:24 +0100

    Initial Commit

---
+++ githot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githot
+//
+//  Created by Mijo Kaliger on 3/1/18.
+//  Copyright © 2018 AirCutDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
